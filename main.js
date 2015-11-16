062efc97702a8683698b17b1027519af2c526291
Ronan <ronanir600@gmail.com>
2015-11-16 14:23:35 +0000

    frst push]

---
+++ main.js
@@ -0,0 +1,440 @@
+$(function() {
+
+// ***********************************
+		 // Location Button
+// ************************************
+	$('.location').on('click', function() {
+		$('.location').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.location').on('click', function() {
+		$('.accommodation').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.location').on('click', function() {
+		$('.flights').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.location').on('click', function() {
+		$('.holiday').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.location').on('click', function() {
+		$('.other').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.location').on('click', function() {
+		$('.rsvp').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+// ***********************************
+		 // Accommodation Button
+// ************************************
+
+
+	$('.accommodation').on('click', function() {
+		$('.accommodation').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.location').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.flights').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.activities').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.other').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.rsvp').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+
+// ***********************************
+		 // Flights Button
+// ************************************
+
+
+	$('.flights').on('click', function() {
+		$('.flights').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.location').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.accommodation').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.activities').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.other').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.rsvp').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+// ***********************************
+		 // Activities Button
+// ************************************
+
+
+	$('.holiday').on('click', function() {
+		$('.holiday').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.location').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.accommodation').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.flights').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.other').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.rsvp').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+// ***********************************
+		 // RSVP Button
+// ************************************
+
+
+	$('.rsvp').on('click', function() {
+		$('.rsvp').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.rsvp').on('click', function() {
+		$('.location').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.rsvp').on('click', function() {
+		$('.accommodation').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.rsvp').on('click', function() {
+		$('.flights').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+
+	$('.rsvp').on('click', function() {
+		$('.other').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.rsvp').on('click', function() {
+		$('.holiday').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+// ***********************************
+		 // Other
+// ************************************
+
+
+	$('.other').on('click', function() {
+		$('.other').css({
+			'color' : 'white', 
+			'background-color' : '#5fcf80'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.location').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.accommodation').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.flights').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.holiday').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.rsvp').css({
+			'color' : '#5fcf80', 
+			'background-color' : 'white'
+		})
+	})
+
+// ***********************************
+		 // Toggling Between Buttons
+// ************************************
+
+	$('.location').on('click', function() {
+		$('.location-box').css({
+			'display' : 'block'
+		})
+		$('.accommodation-box').css({
+			'display' : 'none'
+		})
+		$('.travel-box').css({
+			'display' : 'none'
+		})
+		$('.other-box').css({
+			'display' : 'none'
+		})
+		$('.holiday-box').css({
+			'display' : 'none'
+		})
+		$('.rsvp-box').css({
+			'display' : 'none'
+		})
+	})
+
+	$('.accommodation').on('click', function() {
+		$('.location-box').css({
+			'display' : 'none'
+		})
+		$('.accommodation-box').css({
+			'display' : 'block'
+		})
+		$('.travel-box').css({
+			'display' : 'none'
+		})
+		$('.other-box').css({
+			'display' : 'none'
+		})
+		$('.holiday-box').css({
+			'display' : 'none'
+		})
+		$('.rsvp-box').css({
+			'display' : 'none'
+		})
+	})
+
+	$('.flights').on('click', function() {
+		$('.location-box').css({
+			'display' : 'none'
+		})
+		$('.accommodation-box').css({
+			'display' : 'none'
+		})
+		$('.travel-box').css({
+			'display' : 'block'
+		})
+		$('.other-box').css({
+			'display' : 'none'
+		})
+		$('.holiday-box').css({
+			'display' : 'none'
+		})
+		$('.rsvp-box').css({
+			'display' : 'none'
+		})
+	})
+
+	$('.other').on('click', function() {
+		$('.location-box').css({
+			'display' : 'none'
+		})
+		$('.accommodation-box').css({
+			'display' : 'none'
+		})
+		$('.travel-box').css({
+			'display' : 'none'
+		})
+		$('.other-box').css({
+			'display' : 'block'
+		})
+		$('.holiday-box').css({
+			'display' : 'none'
+		})
+		$('.rsvp-box').css({
+			'display' : 'none'
+		})
+	})
+
+	$('.holiday').on('click', function() {
+		$('.location-box').css({
+			'display' : 'none'
+		})
+		$('.accommodation-box').css({
+			'display' : 'none'
+		})
+		$('.travel-box').css({
+			'display' : 'none'
+		})
+		$('.other-box').css({
+			'display' : 'none'
+		})
+		$('.holiday-box').css({
+			'display' : 'block'
+		})
+		$('.rsvp-box').css({
+			'display' : 'none'
+		})
+	})
+
+	$('.rsvp').on('click', function() {
+		$('.location-box').css({
+			'display' : 'none'
+		})
+		$('.accommodation-box').css({
+			'display' : 'none'
+		})
+		$('.travel-box').css({
+			'display' : 'none'
+		})
+		$('.other-box').css({
+			'display' : 'none'
+		})
+		$('.holiday-box').css({
+			'display' : 'none'
+		})
+		$('.rsvp-box').css({
+			'display' : 'block'
+		})
+	})
+
+/*******************************
+		    Ajax Post
+*******************************/
+
+	$('#submit').on('click', function(e) {
+		// e.preventDefault();
+
+		$.ajax({
+			method: 'POST',
+			url: '/rsvp',
+			data: {
+				name: $('#name').val(),
+				email: $('#inputEmail').val(),
+				guest1: $('#inlineRadio1').val(),
+				guest2: $('#inlineRadio2').val(),
+				guest3: $('#inlineRadio3').val(),
+				guest4: $('#inlineRadio4').val(),
+				comments: $('#comments').val()
+			}
+	
+		})
+	})
+
+
+
+})
